9e2d3c6730cee63fc797f5423cde15c8034b07b0
airotii <57942711+airotii@users.noreply.github.com>
2020-06-23 17:38:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 // const formatMessage = require('messages.js');
-const {
-    userJoin,
-    getCurrentUser,
-    userLeave,
-    getRoomUsers
-} = require('./utils/users');
+// const {
+//     userJoin,
+//     getCurrentUser,
+//     userLeave,
+//     getRoomUsers
+// } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
